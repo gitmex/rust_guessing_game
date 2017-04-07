03ce1e8e4152868fb0f599c3df844919bcc7ed2f
Chris Malinowski <christoph.malinowski@intralot.de>
2017-04-07 16:08:41 +0200

    task one from the example

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Lisa macht die Ansagen!");
+	let mut ansage = String::new();
+	io::stdin().read_line(&mut ansage).expect("Lisa machte keine Ansagen =(");
+	println!("Simon says: {}", ansage);
 }
