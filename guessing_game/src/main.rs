a6d173e43359e60903914f264f797099dac07845
Chris Malinowski <christoph.malinowski@intralot.de>
2017-04-07 16:27:22 +0200

    middle of the tutorial

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,30 @@
+extern crate rand;
+
+
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 
 
 fn main() {
     println!("Lisa macht die Ansagen!");
 	let mut ansage = String::new();
+	
+	let secret_number = rand::thread_rng().gen_range(1, 101);
+	
+    println!("The secret number is: {}", secret_number);
+	
+	
+	
 	io::stdin().read_line(&mut ansage).expect("Lisa machte keine Ansagen =(");
-	println!("Simon says: {}", ansage);
+
+	let ansage: u32 = ansage.trim().parse()
+	        .expect("Please type a number!");
+    
+	match ansage.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You win!"),
+    }
 }
